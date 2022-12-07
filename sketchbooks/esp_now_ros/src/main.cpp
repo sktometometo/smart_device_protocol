a1b14a325471dd37bd7975afb8dc2350a640b66d
Koki Shinjo <sktometometo@gmail.com>
2022-12-07 17:24:07 +0900

    Update Packet.msg and esp_now_ros sketch

--- sketchbooks/esp_now_ros/src/main.cpp
+++ sketchbooks/esp_now_ros/src/main.cpp
@@ -5,18 +5,41 @@
 #include <ros.h>
 #include <esp_now_ros/Packet.h>
 
+void messageCb(const esp_now_ros::Packet&);
+
 ros::NodeHandle nh;
 esp_now_ros::Packet msg_recv_packet;
 ros::Publisher publisher("~recv", &msg_recv_packet);
+ros::Subscriber<esp_now_ros::Packet> subscriber("~send", &messageCb);
+
+void messageCb(const esp_now_ros::Packet& msg) {
+  // Register peer
+  esp_now_peer_info_t peer_temp;
+  memset(&peer_temp, 0, sizeof(peer_temp));
+  for (int i=0; i<6; i++) {
+      peer_temp.peer_addr[i] = msg.mac_address[i];
+  }
+  esp_err_t add_status = esp_now_add_peer(&peer_temp);
+
+  // Send data
+  esp_err_t result = esp_now_send(peer_temp.peer_addr, (uint8_t*)msg.data, msg.data_length);
+
+  // Unregister peer
+  esp_err_t del_status = esp_now_del_peer(peer_temp.peer_addr);
+}
 
 
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
   char mac_str[18];
-  char buffer[256];
+  uint8_t buffer[256];
   snprintf(mac_str, sizeof(mac_str), "%02X:%02X:%02X:%02X:%02X:%02X",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  msg_recv_packet.mac_address = mac_str;
-  msg_recv_packet.mac_address_length = sizeof(mac_str);
+  uint8_t mac_address[6];
+  for ( int i=0; i<6; i++ ) {
+      mac_address[i] = mac_addr[i];
+  }
+  msg_recv_packet.mac_address = mac_address;
+  msg_recv_packet.mac_address_length = 6;
   msg_recv_packet.data = buffer;
   msg_recv_packet.data_length = data_len;
   publisher.publish(&msg_recv_packet);
@@ -26,8 +49,9 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
 
 void setup()
 {
-  nh.initNode("ESP_NOW_ROS");
+  nh.initNode();
   nh.advertise(publisher);
+  nh.subscribe(subscriber);
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
