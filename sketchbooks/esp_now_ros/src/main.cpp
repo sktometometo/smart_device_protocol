d8372bf31aaff7437f1c09a1bdc7f44b1765f112
Koki Shinjo <sktometometo@gmail.com>
2022-12-07 18:06:12 +0900

    Update esp_now_ros sketch

--- sketchbooks/esp_now_ros/src/main.cpp
+++ sketchbooks/esp_now_ros/src/main.cpp
@@ -1,19 +1,33 @@
 #include <esp_now.h>
+#include <esp_system.h>
 #include <M5Stack.h>
 #include <WiFi.h>
 
+#define LGFX_M5STACK
+#define LGFX_USE_V1
+#include <LovyanGFX.hpp>
+#include <LGFX_AUTODETECT.hpp>
+
+#define ESP_SERIAL // Use USB-Rosserial
 #include <ros.h>
 #include <esp_now_ros/Packet.h>
 
 void messageCb(const esp_now_ros::Packet&);
 
-ros::NodeHandle nh;
+static LGFX lcd;
+static LGFX_Sprite sprite_device_info(&lcd);
+static LGFX_Sprite sprite_event_info(&lcd);
+
 esp_now_ros::Packet msg_recv_packet;
+uint8_t mac_address_for_msg[6];
+uint8_t buffer_for_msg[256];
+
+ros::NodeHandle nh;
 ros::Publisher publisher("~recv", &msg_recv_packet);
 ros::Subscriber<esp_now_ros::Packet> subscriber("~send", &messageCb);
 
 void messageCb(const esp_now_ros::Packet& msg) {
-  // Register peer
+  // Register a peer
   esp_now_peer_info_t peer_temp;
   memset(&peer_temp, 0, sizeof(peer_temp));
   for (int i=0; i<6; i++) {
@@ -24,35 +38,115 @@ void messageCb(const esp_now_ros::Packet& msg) {
   // Send data
   esp_err_t result = esp_now_send(peer_temp.peer_addr, (uint8_t*)msg.data, msg.data_length);
 
-  // Unregister peer
+  // Unregister the peer
   esp_err_t del_status = esp_now_del_peer(peer_temp.peer_addr);
+
+  // Display
+  sprite_event_info.fillScreen(0xFFFFFF);
+  sprite_event_info.setCursor(0, 0);
+  sprite_event_info.println("send packet");
+  sprite_event_info.printf(
+          "target mac: %02X:%02X:%02X:%02X:%02X:%02X\n",
+          msg.mac_address[0],
+          msg.mac_address[1],
+          msg.mac_address[2],
+          msg.mac_address[3],
+          msg.mac_address[4],
+          msg.mac_address[5]
+          );
+  sprite_event_info.print("data: ");
+  for ( int i=0; i<msg.data_length; i++ ) {
+      sprite_event_info.printf("%d ", msg.data[i]);
+  }
+  sprite_event_info.println("");
+  sprite_event_info.pushSprite(0, 80);
 }
 
 
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
-  char mac_str[18];
-  uint8_t buffer[256];
-  snprintf(mac_str, sizeof(mac_str), "%02X:%02X:%02X:%02X:%02X:%02X",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  uint8_t mac_address[6];
   for ( int i=0; i<6; i++ ) {
-      mac_address[i] = mac_addr[i];
+      mac_address_for_msg[i] = mac_addr[i];
   }
-  msg_recv_packet.mac_address = mac_address;
+  for ( int i=0; i<data_len; i++ ) {
+      buffer_for_msg[i] = data[i];
+  }
+  msg_recv_packet.mac_address = mac_address_for_msg;
   msg_recv_packet.mac_address_length = 6;
-  msg_recv_packet.data = buffer;
+  msg_recv_packet.data = buffer_for_msg;
   msg_recv_packet.data_length = data_len;
   publisher.publish(&msg_recv_packet);
   nh.spinOnce();
+
+  // Display
+  sprite_event_info.fillScreen(0xFFFFFF);
+  sprite_event_info.setCursor(0, 0);
+  sprite_event_info.println("recieve packet");
+  sprite_event_info.printf(
+          "src mac: %02X:%02X:%02X:%02X:%02X:%02X\n",
+          mac_address_for_msg[0],
+          mac_address_for_msg[1],
+          mac_address_for_msg[2],
+          mac_address_for_msg[3],
+          mac_address_for_msg[4],
+          mac_address_for_msg[5]
+          );
+  sprite_event_info.print("data: ");
+  for ( int i=0; i<data_len; i++ ) {
+      sprite_event_info.printf("%d ", data[i]);
+  }
+  sprite_event_info.println("");
+  sprite_event_info.pushSprite(0, 80);
 }
 
 
 void setup()
 {
+  // Read device mac address
+  uint8_t device_mac_address[6] = {0};
+  esp_read_mac(device_mac_address, ESP_MAC_WIFI_STA);
+
+  // LCD Initialization
+  lcd.init();
+  lcd.setRotation(1);
+  lcd.setBrightness(128);
+  lcd.setColorDepth(24);
+  lcd.fillScreen(0xFFFFFF);
+
+  sprite_device_info.createSprite(320, 80);
+  sprite_event_info.createSprite(320, 160);
+
+  sprite_device_info.fillScreen(0xFFFFFF);
+  sprite_device_info.setTextColor(0x000000);
+  sprite_device_info.setTextSize(1.5, 1.5);
+  sprite_event_info.fillScreen(0xFFFFFF);
+  sprite_event_info.setTextColor(0x000000);
+
+  sprite_device_info.println("ESP-NOW ROS Driver");
+  sprite_device_info.printf(
+          "MAC ADDR: %02X:%02X:%02X:%02X:%02X:%02X\n",
+          device_mac_address[0],
+          device_mac_address[1],
+          device_mac_address[2],
+          device_mac_address[3],
+          device_mac_address[4],
+          device_mac_address[5]
+          );
+  sprite_device_info.pushSprite(0, 0);
+
+  // Rosserial Initialization
   nh.initNode();
   nh.advertise(publisher);
   nh.subscribe(subscriber);
+  sprite_device_info.println("ROSSERIAL connecting...");
+  sprite_device_info.pushSprite(0, 0);
+  while (not nh.connected()) {
+      delay(1000);
+      nh.spinOnce();
+  }
+  sprite_device_info.println("ROSSERIAL Initialized.");
+  sprite_device_info.pushSprite(0, 0);
 
+  // ESP-NOW initialization
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   if (esp_now_init() == ESP_OK) {
@@ -61,12 +155,12 @@ void setup()
     nh.logerror("ESPNow Init Failed");
     ESP.restart();
   }
-
   esp_now_register_recv_cb(OnDataRecv);
+  sprite_device_info.println("ESP-NOW Initialized.");
+  sprite_device_info.pushSprite(0, 0);
 }
 
 void loop() {
-  while (nh.connected()) {
-    nh.spinOnce();
-  }
+  nh.spinOnce();
+  delay(5);
 }
