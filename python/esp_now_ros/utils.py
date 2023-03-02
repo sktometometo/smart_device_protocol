adea41a276d2619d9ae8021a9cfb3cee69011dd6
Koki Shinjo <sktometometo@gmail.com>
2023-03-02 18:06:50 +0900

    [esp_now_ros] add utils.py

---
+++ python/esp_now_ros/utils.py
@@ -0,0 +1,39 @@
+import rospy
+from esp_now_ros.msg import Packet
+import struct
+
+
+def create_test_packet():
+
+  return struct.pack('H', Packet.PACKET_TYPE_TEST) + struct.pack(
+      'i', -120) + struct.pack('f', -1.0) + struct.pack(
+          '64s', 'Hello, world!'.encode('utf-8'))
+
+
+class ESPNOWROSInterface:
+
+  def __init__(self, calback=None):
+
+    self.raw_callback = callback
+    self.sub = rospy.Subscriber('/esp_now_ros/recv', Packet, self.callback)
+    self.pub = rospy.Publisher('/esp_now_ros/send', Packet, queue_size=1)
+
+  def callback(self, msg):
+
+    src_address = struct.unpack('6B', msg.mac_address)
+    data = msg.data
+    if self.raw_callback is not None:
+      self.raw_callback(src_address, data)
+
+  def send(self, target_address, data):
+    """
+    Args:
+        target_address (list of int)
+        data (bytes)
+    """
+    msg = Packet()
+    msg.mac_address = struct.pack('6B', target_address[0], target_address[1],
+                                  target_address[2], target_address[3],
+                                  target_address[4], target_address[5])
+    msg.data = data
+    self.pub.publish(msg)
