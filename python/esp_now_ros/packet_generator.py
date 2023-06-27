72d292a96cb9d36c649017e8641d9f9e9dc89a44
Koki Shinjo <sktometometo@gmail.com>
2023-06-27 14:32:54 +0900

    Add byte order flag to python packet generator/parser

--- python/esp_now_ros/packet_generator.py
+++ python/esp_now_ros/packet_generator.py
@@ -7,9 +7,9 @@ def create_test_packet(num_int: int = -120,
                        num_float: float = -1.0,
                        string: str = "Hello, world!"):
 
-  return struct.pack('H', Packet.PACKET_TYPE_TEST) + \
-         struct.pack('i', num_int) + \
-         struct.pack('f', num_float) + \
+  return struct.pack('<H', Packet.PACKET_TYPE_TEST) + \
+         struct.pack('<i', num_int) + \
+         struct.pack('<f', num_float) + \
          struct.pack('64s', string.encode('utf-8'))
 
 
@@ -18,7 +18,7 @@ def create_task_dispatcher_packet(caller_name: str,
                                   task_name: str,
                                   task_args: str = ''):
 
-  return struct.pack('H', Packet.PACKET_TYPE_TASK_DISPATCHER) + \
+  return struct.pack('<H', Packet.PACKET_TYPE_TASK_DISPATCHER) + \
        struct.pack('16s', caller_name.encode('utf-8')) + \
        struct.pack('16s', target_name.encode('utf-8')) + \
        struct.pack('16s', task_name.encode('utf-8')) + \
@@ -28,7 +28,7 @@ def create_task_dispatcher_packet(caller_name: str,
 def create_task_received_packet(worker_name: str, caller_name: str,
                                 task_name: str):
 
-  return struct.pack('H', Packet.PACKET_TYPE_TASK_RECEIVED) + \
+  return struct.pack('<H', Packet.PACKET_TYPE_TASK_RECEIVED) + \
        struct.pack('16s', worker_name.encode('utf-8')) + \
        struct.pack('16s', caller_name.encode('utf-8')) + \
        struct.pack('16s', task_name.encode('utf-8'))
@@ -39,18 +39,18 @@ def create_emergency_packet(map_frame: str, position_x: float,
                             orientation_x: float, orientation_y: float,
                             orientation_z: float, orientation_w: float):
 
-  return struct.pack('H', Packet.PACKET_TYPE_EMERGENCY) + \
+  return struct.pack('<H', Packet.PACKET_TYPE_EMERGENCY) + \
       struct.pack('64s', map_frame.encode('utf-8')) + \
-      struct.pack('f', position_x) + \
-      struct.pack('f', position_y) + \
-      struct.pack('f', position_z) + \
-      struct.pack('f', orientation_x) + \
-      struct.pack('f', orientation_y) + \
-      struct.pack('f', orientation_z) + \
-      struct.pack('f', orientation_w)
+      struct.pack('<f', position_x) + \
+      struct.pack('<f', position_y) + \
+      struct.pack('<f', position_z) + \
+      struct.pack('<f', orientation_x) + \
+      struct.pack('<f', orientation_y) + \
+      struct.pack('<f', orientation_z) + \
+      struct.pack('<f', orientation_w)
 
 def create_device_message_board_data(source_name: str, timeout_duration: int, message: str):
-  return struct.pack('H', Packet.PACKET_TYPE_DEVICE_MESSAGE_BOARD_DATA) + \
+  return struct.pack('<H', Packet.PACKET_TYPE_DEVICE_MESSAGE_BOARD_DATA) + \
       struct.pack('64s', source_name.encode('utf-8')) + \
-      struct.pack('L', int(timeout_duration)) + \
+      struct.pack('<L', int(timeout_duration)) + \
       struct.pack('64s', message.encode('utf-8'))
