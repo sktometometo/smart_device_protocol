3b7ba59504ed6887301e6fc72c07765f9e40f7e7
Koki Shinjo <sktometometo@gmail.com>
2023-06-27 15:10:16 +0900

    Fix python format with black

--- node_scripts/esp_now_packet_printer.py
+++ node_scripts/esp_now_packet_printer.py
@@ -7,13 +7,11 @@ from esp_now_ros.packet_parser import parse_packet
 
 
 def callback(src_address, data):
+    result = parse_packet(data)
+    rospy.loginfo("Packet from {} : {}".format(src_address, result))
 
-  result = parse_packet(data)
-  rospy.loginfo('Packet from {} : {}'.format(src_address, result))
 
-
-if __name__ == '__main__':
-
-  rospy.init_node('esp_now_packet_printer')
-  interface = ESPNOWROSInterface(callback)
-  rospy.spin()
+if __name__ == "__main__":
+    rospy.init_node("esp_now_packet_printer")
+    interface = ESPNOWROSInterface(callback)
+    rospy.spin()
