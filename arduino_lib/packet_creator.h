32b83ac64a8b582fc781eb4ea47afe6b5b3bb18c
Koki Shinjo <sktometometo@gmail.com>
2023-09-15 16:41:26 +0900

    [arduino_lib] update

--- arduino_lib/packet_creator.h
+++ arduino_lib/packet_creator.h
@@ -1,38 +1,13 @@
 #ifndef ESP_NOW_ROS_PACKET_CREATOR_H__
 #define ESP_NOW_ROS_PACKET_CREATOR_H__
 
+#include <variant>
+#include <vector>
+#include <string.h>
+
 #include <esp_now_ros/Packet.h>
 
-void create_sensor_enviii_packet(uint8_t* packet, const char* module_name, int32_t pressure)
-{
-  *(uint16_t*)(packet + 0) = esp_now_ros::Packet::PACKET_TYPE_SENSOR_ENV_III;
-  strncpy((char*)(packet + 2), module_name, 64);
-  *(int32_t*)(packet + 2 + 64) = pressure;
-}
-
-void create_sensor_stickv2_packet(uint8_t* packet, uint32_t number_of_person, const char* place_name)
-{
-  *(uint16_t*)(packet + 0) = esp_now_ros::Packet::PACKET_TYPE_SENSOR_UNITV2_PERSON_COUNTER;
-  *(uint32_t*)(packet + 2) = number_of_person;
-  strncpy((char*)(packet + 2 + 4), place_name, 64);
-}
-
-void create_device_message_board_meta_packet(uint8_t* packet, const char* module_name)
-{
-  *(uint16_t*)(packet + 0) = esp_now_ros::Packet::PACKET_TYPE_DEVICE_MESSAGE_BOARD_META;
-  strncpy((char*)(packet + 2), module_name, 64);
-}
-
-void create_device_message_board_data_packet(uint8_t* packet, const char* source_name, uint64_t timeout_duration,
-                                             const char* message)
-{
-  *(uint16_t*)(packet + 0) = esp_now_ros::Packet::PACKET_TYPE_DEVICE_MESSAGE_BOARD_DATA;
-  strncpy((char*)(packet + 2), source_name, 64);
-  *(uint64_t*)(packet + 2 + 64) = timeout_duration;
-  strncpy((char*)(packet + 2 + 64 + 8), message, 64);
-}
-
-void generate_meta_frame(uint8_t* packet, const char* device_name, const char* packet_description_01, const char* serialization_format_01, const char* packet_description_02, const char* serialization_format_02, const char* packet_description_03, const char serialization_format_03)
+void generate_meta_frame(uint8_t* packet, const char* device_name, const char* packet_description_01, const char* serialization_format_01, const char* packet_description_02, const char* serialization_format_02, const char* packet_description_03, const char* serialization_format_03)
 {
   *(uint16_t*)(packet + 0) = esp_now_ros::Packet::PACKET_TYPE_META_FRAME;
   strncpy((char*)(packet + 2), device_name, 20);
@@ -44,15 +19,43 @@ void generate_meta_frame(uint8_t* packet, const char* device_name, const char* p
   strncpy((char*)(packet + 2 + 20 + 64 + 10 + 64 + 10 + 64), serialization_format_03, 10);
 }
 
-void generate_data_frame(uint8_t* packet, const char* packet_description, const char* serialization_format, std::vector<std::variant<int, float, std::string, bool>> data)
+void generate_data_frame(uint8_t* packet, const char* packet_description, const char* serialization_format, std::vector<std::variant<int32_t, float, std::string, bool>> data)
 {
   *(uint16_t*)(packet + 0) = esp_now_ros::Packet::PACKET_TYPE_DATA_FRAME;
   strncpy((char*)(packet + 2), packet_description, 64);
   strncpy((char*)(packet + 2 + 64), serialization_format, 10);
+  auto packet_data_p = packet + 2 + 64 + 10;
   for (auto = data.begin(); it != data.end(); ++it) {
-    
+    if (std::holds_alternative<int32_t>(*it)) {
+      *(int32_t*)packet_data_p = std::get<int32_t>(*it);
+      packet_data_p += sizeof(int32_t);
+    } else if (std::holds_alternative<float>(*it)) {
+      *(float*)packet_data_p = std::get<float>(*it);
+      packet_data_p += sizeof(float);
+    } else if (std::holds_alternative<std::string>(*it)) {
+      std::string str = std::get<std::string>(*it);
+      if (str.size() > 64) {
+        str.resize(64);
+        strncpy((char*)packet_data_p, str.c_str(), 64);
+        packet_data_p += 64;
+      } else if (str.size() <= 64 and str.size() > 16) {
+        strncpy((char*)packet_data_p, str.c_str(), str.size());
+        for (int i = str.size(); i < 64; ++i) {
+          *(char*)(packet + 2 + 64 + 10 + i) = '\0';
+        }
+        packet_data_p += 64;
+      } else if (str.size() <= 16) {
+        strncpy((char*)packet_data_p, str.c_str(), str.size());
+        for (int i = str.size(); i < 16; ++i) {
+          *(char*)(packet + 2 + 64 + 10 + i) = '\0';
+        }
+        packet_data_p += 16;
+      }
+    } else if (std::holds_alternative<bool>(*it)) {
+      *(bool*)packet_data_p = std::get<bool>(*it);
+      packet_data_p += sizeof(bool);
+    }
   }
-  // TODO
 }
 
 #endif
--- arduino_lib/packet_parser.h
+++ arduino_lib/packet_parser.h
@@ -3,41 +3,49 @@
 
 #include <esp_now_ros/Packet.h>
 #include <string.h>
+#include <tuple>
+#include <variant>
 
 uint16_t get_packet_type(const uint8_t* packet)
 {
   return *(uint16_t*)packet;
 }
 
-void parse_packet_as_test_packet(const uint8_t* packet, uint16_t& packet_type, int32_t& num_int, float num_float,
-                                 char* str)
+std::tuple<std::string, std::vector<std::tuple<std::string, std::string>>> parse_packet_as_meta_packet(const uint8_t* packet)
 {
-  packet_type = *(uint16_t*)packet;
-  num_int = *(int32_t*)(packet + 2);
-  num_float = *(float*)(packet + 2 + 4);
-  strncpy(str, (char*)(packet + 2 + 4 + 4), 64);
+  std::string device_name;
+  std::vector<std::tuple<std::string, std::string>> packet_description_and_serialization_format;
+  device_name = std::string((char*)(packet + 2), 20);
+  for (int i = 0; i < 3; ++i) {
+    std::string packet_description = std::string((char*)(packet + 2 + 20 + 74 * i), 64);
+    std::string serialization_format = std::string((char*)(packet + 2 + 20 + 74 * i + 10), 10);
+    packet_description_and_serialization_format.push_back(std::make_tuple(packet_description, serialization_format));
+  }
+  return std::make_tuple(device_name, packet_description_and_serialization_format);
 }
 
-void parse_packet_as_named_string_packet(const uint8_t* packet, uint16_t& packet_type, char* name, char* value)
+std::tuple<std::string, std::string, std::vector<std::variant<int32_t, float, std::string, bool>>> parse_packet_as_data_packet(const uint8_t* packet)
 {
-  packet_type = *(uint16_t*)packet;
-  strncpy(name, (char*)(packet + 2), 64);
-  strncpy(value, (char*)(packet + 2 + 64), 64);
-}
-
-void parse_packet_as_message_board_meta_packet(const uint8_t* packet, uint16_t& packet_type, char* module_name)
-{
-  packet_type = *(uint16_t*)packet;
-  strncpy(module_name, (char*)(packet + 2), 64);
-}
-
-void parse_packet_as_message_board_data_packet(const uint8_t* packet, uint16_t& packet_type, char* source_name,
-                                               uint64_t& timeout_duration, char* message)
-{
-  packet_type = *(uint16_t*)packet;
-  strncpy(source_name, (char*)(packet + 2), 64);
-  timeout_duration = *(uint64_t*)(packet + 2 + 64);
-  strncpy(message, (char*)(packet + 2 + 64 + 8), 64);
+  std::string packet_description = std::string((char*)(packet + 2), 64);
+  std::string serialization_format = std::string((char*)(packet + 2 + 64), 10);
+  std::vector<std::variant<int32_t, float, std::string, bool>> data;
+  auto packet_data_p = packet + 2 + 64 + 10;
+  for (int i = 0; i < serialization_format.size(); ++i) {
+    if (serialization_format[i] == 'i') {
+      data.push_back(std::get<int32_t>(*(int32_t*)packet_data_p));
+      packet_data_p += sizeof(int32_t);
+    } else if (serialization_format[i] == 'f') {
+      data.push_back(std::get<float>(*(float*)packet_data_p));
+      packet_data_p += sizeof(float);
+    } else if (serialization_format[i] == 's') {
+      std::string str = std::string((char*)packet_data_p, 64);
+      data.push_back(str);
+      packet_data_p += 64;
+    } else if (serialization_format[i] == '?') {
+      data.push_back(std::get<bool>(*(bool*)packet_data_p));
+      packet_data_p += sizeof(bool);
+    }
+  }
 }
 
 #endif
