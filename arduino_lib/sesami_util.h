eb49ebfd309112019e50c21865d059f1efe990e8
Koki Shinjo <sktometometo@gmail.com>
2023-09-25 09:47:36 +0900

    Merge arduino_lib

--- arduino_lib/sesami_util.h
+++ arduino_lib/sesami_util.h
@@ -1,3 +1,5 @@
+#ifndef SESAMI_UTIL_H
+#define SESAMI_UTIL_H
 
 #include <M5AtomS3.h>
 #include <WiFi.h>
@@ -9,7 +11,15 @@
 
 #include <base64.h>
 
-#include "sesami_util.h"
+#include <optional>
+
+String generateRandomTag(String secret_key, uint32_t date_sec);
+
+std::optional<String> operation_sesami(String device_uuid, String api_key, int command, String secret_key);
+
+std::optional<String> get_sesami_status(String device_uuid, String api_key);
+
+std::optional<String> get_sesami_history(String device_uuid, String api_key);
 
 String generateRandomTag(String secret_key, uint32_t date_sec)
 {
@@ -166,3 +176,5 @@ std::optional<String> get_sesami_history(String device_uuid, String api_key)
 
     return body;
 }
+
+#endif // SESAMI_UTIL_H
