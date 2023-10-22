0bd8147384f59b8c1c98fbf18fa1a455f3601fda
Koki Shinjo <sktometometo@gmail.com>
2023-10-22 17:30:46 +0900

    [sdp_switchbot_*] update

--- arduino_lib/iot_com_util/iot_host_util.h
+++ arduino_lib/iot_com_util/iot_host_util.h
@@ -18,4 +18,18 @@ String send_serial_command(String command, int timeout_duration = 5000)
   return "";
 }
 
+void clear_recv_buf(int duration = 5000)
+{
+  auto timeout = millis() + duration;
+  while (millis() < timeout)
+  {
+    delay(100);
+    if (Serial2.available())
+    {
+      Serial2.readStringUntil('\n');
+    }
+  }
+  return;
+}
+
 #endif // IOT_HOST_UTIL_H
