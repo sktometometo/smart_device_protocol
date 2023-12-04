f4bd9cb2dbe0791e28a46754bc5195344773a304
Koki Shinjo <sktometometo@gmail.com>
2023-12-04 16:17:23 +0900

    [sdp_landmark_information] update

--- sketchbooks/sdp_landmark_information/src/main.cpp
+++ sketchbooks/sdp_landmark_information/src/main.cpp
@@ -1,7 +1,11 @@
 #include <vector>
 #include <variant>
 
+#if defined(M5STACK_FIRE)
 #include <M5Stack.h>
+#elif defined(M5STACK_CORE2)
+#include <M5Core2.h>
+#endif
 #include <FS.h>
 #include <SPIFFS.h>
 
@@ -17,7 +21,7 @@
 String device_name = "";
 
 // ESP-NOW
-uint8_t mac_address[6] = {0};
+uint8_t mac_address[6] = { 0 };
 
 // SDP Interface
 std::string packet_description_information = "Landmark information";
@@ -34,7 +38,7 @@ std::vector<SDPData> data_for_uwb_data_packet;
 std::vector<SDPData> data;
 int loop_counter = 0;
 
-bool load_config_from_FS(fs::FS &fs, String filename = "/config.json")
+bool load_config_from_FS(fs::FS& fs, String filename = "/config.json")
 {
   StaticJsonDocument<1024> doc;
   if (not load_json_from_FS<1024>(fs, filename, doc))
@@ -92,20 +96,30 @@ void setup()
   Serial.println("SDP Initialized!");
 
   // UWB module
+#if defined(M5STACK_FIRE)
   Serial1.begin(115200, SERIAL_8N1, 16, 17);
+#elif defined(M5STACK_CORE2)
+  Serial1.begin(115200, SERIAL_8N1, 33, 32);
+#endif
   bool result = initUWB(false, uwb_id, Serial1);
   data_for_uwb_data_packet.push_back(SDPData(uwb_id));
-  if (result) {
+  if (result)
+  {
     M5.lcd.printf("Success for initialization of UWB\n");
-  } else {
+  }
+  else
+  {
     M5.lcd.printf("Failed to initialize UWB\n");
   }
 
   // Display MAC address
   M5.Lcd.printf("Name: %s\n", device_name.c_str());
-  M5.Lcd.printf("ADDR: %2x:%2x:%2x:%2x:%2x:%2x\n",
-                mac_address[0], mac_address[1], mac_address[2],
-                mac_address[3], mac_address[4], mac_address[5]);
+  M5.Lcd.printf("ADDR: %2x:%2x:%2x:%2x:%2x:%2x\n", mac_address[0], mac_address[1], mac_address[2], mac_address[3],
+                mac_address[4], mac_address[5]);
+
+  // Display loaded config
+  M5.Lcd.printf("UWB ID: %d\n", uwb_id);
+  M5.Lcd.printf("Information: %s\n", data_for_information_data_packet[0].get<std::string>().c_str());
 }
 
 void loop()
