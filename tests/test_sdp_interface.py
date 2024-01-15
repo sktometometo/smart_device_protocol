8ab2a036e836d87152b44ddfb9d89cd7a9a38f55
Koki Shinjo <sktometometo@gmail.com>
2024-01-15 19:47:18 +0900

    {tests] fix

--- tests/test_sdp_interface.py
+++ tests/test_sdp_interface.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import time
 import unittest
+
 import rospy
 import rostest
 from smart_device_protocol.smart_device_protocol_interface import (
@@ -9,6 +11,7 @@ from smart_device_protocol.smart_device_protocol_interface import (
 
 
 class TestCase(unittest.TestCase):
+
     def callback_test(self, src_address, content):
         print("{}: {}".format(src_address, content))
         self.called = True
@@ -18,13 +21,14 @@ class TestCase(unittest.TestCase):
         rospy.init_node("test_sdp_interface")
         sdp_interface = UWBSDPInterface()
         sdp_interface.register_callback(("Light status", "?"), self.callback_test)
-        rospy.sleep(5)
+        time.sleep(15)
         self.assertTrue(len(sdp_interface.device_interfaces) > 0)
         self.assertTrue(self.called)
         sdp_interface.unregister_callback(("Light status", "?"))
         self.called = False
-        rospy.sleep(5)
+        time.sleep(15)
         self.assertFalse(self.called)
+        self.assertTrue(sdp_interface.device_interfaces[(120, 33, 132, 168, 0, 196)]['distance'] is not None)
 
 
 if __name__ == "__main__":
