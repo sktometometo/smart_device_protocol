41e5057d639de43253c7bb1facc22d7a54fcf417
Koki Shinjo <sktometometo@gmail.com>
2024-01-25 17:57:17 +0900

    Update packet_printer

--- node_scripts/sdp_v2_packet_printer.py
+++ node_scripts/sdp_v2_packet_printer.py
@@ -3,14 +3,27 @@
 import rospy
 
 from smart_device_protocol.smart_device_protocol_interface import SDPInterface
+from smart_device_protocol.utils import address_tuple_to_str, address_str_to_tuple
 
 
-def callback(src_address, frame):
-    print("{} Packet from {}".format(type(frame), src_address))
-    print("{}".format(frame))
+class Node:
+
+    def __init__(self, target_address_str = None):
+        self.target_address = address_str_to_tuple(target_address_str) if target_address_str is not None else None
+        self.interface = SDPInterface(callback_data=self.callback, callback_meta=self.callback)
+
+    def callback(self, src_address, frame):
+        if self.target_address is not None:
+            if src_address == self.target_address:
+                print("{} Packet from {}".format(type(frame), address_tuple_to_str(src_address)))
+                print("{}".format(frame))
+        else:
+            print("{} Packet from {}".format(type(frame), address_tuple_to_str(src_address)))
+            print("{}".format(frame))
 
 
 if __name__ == "__main__":
     rospy.init_node("smart_device_protocol_packet_printer")
-    interface = SDPInterface(callback_data=callback, callback_meta=callback)
+    target_address = rospy.get_param('~target_address', None)
+    node = Node(target_address_str=target_address)
     rospy.spin()
