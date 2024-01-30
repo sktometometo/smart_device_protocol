f25104dee02cc8dd57b4c67ded9a1462dd9258a6
Koki Shinjo <sktometometo@gmail.com>
2024-01-30 11:47:41 +0900

    [smart_device_protocol_interface] increase buffer size

--- sketchbooks/smart_device_protocol_interface/src/main.cpp
+++ sketchbooks/smart_device_protocol_interface/src/main.cpp
@@ -10,9 +10,10 @@
 #include "lcd.h"
 
 // ROS
-#include "ros/node_handle.h"
 #include <smart_device_protocol/Packet.h>
 #include <smart_device_protocol/UWBDistance.h>
+
+#include "ros/node_handle.h"
 #if defined(M5STACKATOMS3)
 #include "ArduinoAtomS3Hardware.h"
 #else
@@ -20,8 +21,9 @@
 #endif
 
 #include <WiFi.h>
-#include "sdp/esp_now.h"
+
 #include "devices/uwb_module_util.h"
+#include "sdp/esp_now.h"
 
 void messageCb(const smart_device_protocol::Packet &);
 
@@ -38,15 +40,13 @@ bool uwb_initialized = false;
 // ROSSerial
 smart_device_protocol::Packet msg_recv_packet;
 smart_device_protocol::UWBDistance msg_uwb;
-ros::NodeHandle_<ArduinoHardware> nh;
+ros::NodeHandle_<ArduinoHardware, 25, 25, 2048, 2048> nh;
 ros::Publisher publisher("/smart_device_protocol/recv", &msg_recv_packet);
 ros::Publisher publisher_uwb("/smart_device_protocol/uwb", &msg_uwb);
 ros::Subscriber<smart_device_protocol::Packet> subscriber("/smart_device_protocol/send", &messageCb);
 
-void messageCb(const smart_device_protocol::Packet &msg)
-{
-  if (msg.mac_address_length != 6)
-  {
+void messageCb(const smart_device_protocol::Packet &msg) {
+  if (msg.mac_address_length != 6) {
     nh.logerror("MAC Address length have to be 6.");
     return;
   }
@@ -54,8 +54,7 @@ void messageCb(const smart_device_protocol::Packet &msg)
   // Register a peer
   esp_now_peer_info_t peer_temp;
   memset(&peer_temp, 0, sizeof(peer_temp));
-  for (int i = 0; i < 6; i++)
-  {
+  for (int i = 0; i < 6; i++) {
     peer_temp.peer_addr[i] = msg.mac_address[i];
   }
   esp_err_t add_status = esp_now_add_peer(&peer_temp);
@@ -72,14 +71,11 @@ void messageCb(const smart_device_protocol::Packet &msg)
   nh.logdebug("Subscribe a message and send a packet.");
 }
 
-void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
-{
-  for (int i = 0; i < 6; i++)
-  {
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  for (int i = 0; i < 6; i++) {
     mac_address_for_msg[i] = mac_addr[i];
   }
-  for (int i = 0; i < data_len; i++)
-  {
+  for (int i = 0; i < data_len; i++) {
     buffer_for_msg[i] = data[i];
   }
   msg_recv_packet.mac_address = mac_address_for_msg;
@@ -99,15 +95,13 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
   nh.logdebug("Received a packet and publish a message.");
 }
 
-void setup()
-{
+void setup() {
   // UWB initialization
   Serial2.begin(115200, SERIAL_8N1, uwb_serial_rx_pin, uwb_serial_tx_pin);
 
   // Rosserial Initialization
   nh.initNode();
-  while (not nh.connected())
-  {
+  while (not nh.connected()) {
     delay(1000);
     nh.spinOnce();
   }
@@ -116,33 +110,26 @@ void setup()
   nh.getParam("~tag_id", &tag_id, 1);
 
   // UWB initialization
-  if (tag_id < 0)
-  {
+  if (tag_id < 0) {
     uwb_initialized = initUWB(true, tag_id, Serial2);
-  }
-  else
-  {
+  } else {
     resetUWB(Serial2);
   }
 
   // Subscribe and Publish
   nh.advertise(publisher);
-  if (uwb_initialized)
-  {
+  if (uwb_initialized) {
     nh.advertise(publisher_uwb);
   }
   nh.subscribe(subscriber);
-  while (not nh.connected())
-  {
+  while (not nh.connected()) {
     delay(1000);
     nh.spinOnce();
   }
 
   // ESP-NOW initialization
-  if (not init_esp_now(device_mac_address, OnDataRecv))
-  {
-    while (true)
-    {
+  if (not init_esp_now(device_mac_address, OnDataRecv)) {
+    while (true) {
       delay(1000);
       nh.logerror("ESPNow Init Failed");
     }
@@ -150,14 +137,11 @@ void setup()
 
   // Log
   nh.loginfo("ESPNow Init Success");
-  if (uwb_initialized)
-  {
+  if (uwb_initialized) {
     char buf_str[128];
     sprintf(buf_str, "UWB Init Success. Tag ID: %d", tag_id);
     nh.loginfo(buf_str);
-  }
-  else
-  {
+  } else {
     nh.loginfo("UWB Init Failed. Disabled.");
   }
 
@@ -170,13 +154,10 @@ void setup()
   update_lcd();
 }
 
-void loop()
-{
-  if (uwb_initialized)
-  {
+void loop() {
+  if (uwb_initialized) {
     auto ret = getDistanceUWB(Serial2);
-    if (ret)
-    {
+    if (ret) {
       char buf_for_log[256];
       int id = std::get<0>(*ret);
       float distance = std::get<1>(*ret);
