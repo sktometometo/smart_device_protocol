40c726503d79fbb2f0a104ada9942509419bf1da
Koki Shinjo <sktometometo@gmail.com>
2024-02-01 15:13:59 +0900

    [arduino_lib] update sdp_rpc

--- arduino_lib/sdp/sdp_rpc.h
+++ arduino_lib/sdp/sdp_rpc.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <functional>
+
 #include "sdp/sdp.h"
 
 /*
@@ -10,10 +12,37 @@
  *
  */
 
+// Declarations
+
+typedef void (*sdp_rpc_cb_t)(const uint8_t *mac_addr, const std::vector<SDPData> &request_body, std::vector<SDPData> &response_body);
+
+bool init_sdp_rpc();
+bool register_rpc_service(const SDPInterfaceDescription &request_description, const SDPInterfaceDescription &response_description, sdp_rpc_cb_t callback);
+void unregister_rpc_service(const SDPInterfaceDescription &request_description, const SDPInterfaceDescription &response_description);
+void _request_response_waiting_callback(const uint8_t *mac_addr, const SDPInterfaceDescription &interface_description, const std::vector<SDPData> &body);
+bool call_sdp_rpc(const uint8_t *mac_addr, const SDPInterfaceDescription &request_interface, const std::vector<SDPData> &request_body, const SDPInterfaceDescription &response_interface, std::vector<SDPData> &response_body, unsigned long duration = 3000);
+
+/*
+ * Global variables
+ */
+
+// RPC Server callbacks
+// This dictionary is used to store the callback for each request and used in _request_response_callback
+// Each element is a tuple of (request interface, response interface, callback)
+std::map<SDPInterfaceDescription, std::tuple<SDPInterfaceDescription, sdp_data_if_recv_cb_t>> _rpc_server_callbacks;
+
+// RPC response callback dictionary
+// This dictionary is used to store the response from RPC response for each request and used in _request_response_callback
+std::map<int32_t, std::tuple<std::string, SDPInterfaceDescription, std::vector<SDPData>>> _rpc_response_callback_dictionary;  // request id -> (server_address, response, response body)
+
+/*
+ * RPC Library
+ */
+
 bool init_sdp_rpc() {
   // Register callback for response waiting
   register_sdp_data_callback(_request_response_waiting_callback);
-  register_sdp_data_callback(_rcp_server_base_callback);
+  // register_sdp_data_callback(_rcp_server_base_callback);
   return true;
 }
 
@@ -21,35 +50,13 @@ bool init_sdp_rpc() {
 // RPC Server Library
 //
 
-typedef void (*sdp_rpc_cb_t)(const uint8_t *mac_addr, const std::vector<SDPData> &request_body, std::vector<SDPData> &response_body);
+void _server_callback_base(const uint8_t *mac_addr, const std::vector<SDPData> &body, sdp_rpc_cb_t callback, SDPInterfaceDescription request_interface, SDPInterfaceDescription response_interface) {
+  // Get request id
+  int32_t request_id = std::get<int32_t>(body[0]);
 
-// RPC Server callbacks
-// This dictionary is used to store the callback for each request and used in _request_response_callback
-// Each element is a tuple of (request interface, response interface, callback)
-std::vector<std::tuple<SDPInterfaceDescription, SDPInterfaceDescription, sdp_rpc_cb_t>> _rpc_server_callbacks;
-
-void _rcp_server_base_callback(const uint8_t *mac_addr, const SDPInterfaceDescription &interface_description, const std::vector<SDPData> &request_body) {
-  // Check if there is a callback for the request
-  bool ok = false;
-  SDPInterfaceDescription response_interface;
-  sdp_rpc_cb_t callback;
-  for (int i = 0; i < _rpc_server_callbacks.size(); i++) {
-    if (std::get<0>(_rpc_server_callbacks[i]) == interface_description) {
-      ok = true;
-      response_interface = std::get<1>(_rpc_server_callbacks[i]);
-      callback = std::get<2>(_rpc_server_callbacks[i]);
-      break;
-    }
-  }
-  if (not ok) {
-    return;
-  }
-
-  // Get request body
+  // Get response body
   std::vector<SDPData> response_body;
-
-  // Call callback
-  callback(mac_addr, request_body, response_body);
+  callback(mac_addr, body, response_body);
 
   // Send response
   send_sdp_data_packet(mac_addr, response_interface, response_body);
@@ -57,39 +64,49 @@ void _rcp_server_base_callback(const uint8_t *mac_addr, const SDPInterfaceDescri
 
 bool register_rpc_service(const SDPInterfaceDescription &request_description, const SDPInterfaceDescription &response_description, sdp_rpc_cb_t callback) {
   // Check if request and response serialization formats are valid as RPC
+  std::string request_packet_description = std::get<0>(request_description);
   std::string request_serialization_format = std::get<1>(request_description);
+  std::string response_packet_description = std::get<0>(response_description);
   std::string response_serialization_format = std::get<1>(response_description);
-  if (request_serialization_format[0] != 'i' or response_serialization_format != 'i') {
+  if (request_serialization_format[0] != 'i' or response_serialization_format[0] != 'i') {
     return false;
   }
-  _rpc_server_callbacks.push_back(std::make_tuple(request_description, response_description, callback));
+
+  // bind callback
+  std::function<void(const uint8_t *, const std::vector<SDPData> &)> callback_bind = std::bind(_server_callback_base, std::placeholders::_1, std::placeholders::_2, callback, request_description, response_description);
+  // Register callback
+  register_sdp_interface_callback(request_description, (void *(const uint8_t *mac_addr,
+                                                               const std::vector<SDPData> &body))callback_bind);
+
   return true;
 }
 
-void unregister_rpc_service(const SDPInterfaceDescription &request_description, const SDPInterfaceDescription &response_description) {
-  for (int i = 0; i < _rpc_server_callbacks.size(); i++) {
-    if (std::get<0>(_rpc_server_callbacks[i]) == request_description and
-        std::get<1>(_rpc_server_callbacks[i]) == response_description) {
-      _rpc_server_callbacks.erase(_rpc_server_callbacks.begin() + i);
-      return;
-    }
-  }
+void unregister_rpc_service(const SDPInterfaceDescription &request_description) {
 }
 
 //
 // RPC Client
 //
 
-// RPC response callback dictionary
-// This dictionary is used to store the response from RPC response for each request and used in _request_response_callback
-std::map<int32_t, std::tuple<std::string, SDPInterfaceDescription, std::vector<SDPData>>> _rpc_response_callback_dictionary;  // request id -> (server_address, response, response body)
-
 // callback for response waiting
 void _request_response_waiting_callback(const uint8_t *mac_addr,
                                         const SDPInterfaceDescription
                                             &interface_description,
                                         const std::vector<SDPData> &body) {
-  // Check if the request is registered
+  // Check if there is a callback matches with the response interface description
+  bool ok = false;
+  for (int i = 0; i < _rpc_response_callback_dictionary.size(); i++) {
+    SDPInterfaceDescription response_interface = std::get<1>(_rpc_response_callback_dictionary[i]);
+    if (std::get<0>(response_interface) == std::get<0>(interface_description) and
+        std::get<1>(response_interface) == std::get<1>(interface_description)) {
+      ok = true;
+      break;
+    }
+  }
+  if (not ok) {
+    return;
+  }
+
   int32_t request_id = std::get<int32_t>(body[0]);
   if (_rpc_response_callback_dictionary.find(request_id) ==
       _rpc_response_callback_dictionary.end()) {
@@ -122,12 +139,13 @@ void _request_response_waiting_callback(const uint8_t *mac_addr,
 bool call_sdp_rpc(
     const uint8_t *mac_addr, const SDPInterfaceDescription &request_interface, const std::vector<SDPData> &request_body,
     const SDPInterfaceDescription &response_interface, std::vector<SDPData> &response_body,
-    unsigned long duration = 3000) {
+    unsigned long duration) {
   // Generate request id
   int32_t request_id = random(0, 1000000000);
 
   // Store callback
-  _rpc_response_callback_dictionary[request_id] = std::make_tuple(std::string(mac_addr), response_interface, std::vector<SDPData>());
+  std::string mac_addr_str = _convert_mac_address(mac_addr);
+  _rpc_response_callback_dictionary[request_id] = std::make_tuple(mac_addr_str, response_interface, std::vector<SDPData>());
 
   // Send request
   if (not send_sdp_data_packet(mac_addr, request_interface, request_body)) {
