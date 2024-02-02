74229d159a9cd37c9f44629465d9d7d4df767ebc
Koki Shinjo <sktometometo@gmail.com>
2024-02-02 16:37:31 +0900

    [arduino_lib] update sdp_rpc.h

--- arduino_lib/sdp/sdp_rpc.h
+++ arduino_lib/sdp/sdp_rpc.h
@@ -9,84 +9,148 @@
  *
  * NOTICE:
  *   - The first content of request and response bodies must be request id (integer).
- *
+ *   - The request id is used to match the request and response.
+ *   - The request id must be unique for each request.
  */
 
-// Declarations
-
+/**
+ * Typedefs
+ **/
 typedef void (*sdp_rpc_cb_t)(const uint8_t *mac_addr, const std::vector<SDPData> &request_body, std::vector<SDPData> &response_body);
+typedef std::tuple<SDPInterfaceDescription, SDPInterfaceDescription, sdp_rpc_cb_t> SDPRPCCallbackEntry;
 
+/**
+ * Declarations
+ **/
+
+/**
+ * @brief Initialize SDP RPC library. Before using this library, this function must be called. And before calling this function, SDP library must be initialized.
+ *
+ * @return true if the initialization is successful
+ * @return false if the initialization is failed
+ */
 bool init_sdp_rpc();
+
+/**
+ * @brief Register RPC service to the server. The server will call the callback function when the request is received.
+ *
+ * @param request_description The description of the request interface
+ * @param response_description The description of the response interface
+ * @param callback The callback function
+ * @return true if the registration is successful
+ * @return false if the registration is failed
+ */
 bool register_rpc_service(const SDPInterfaceDescription &request_description, const SDPInterfaceDescription &response_description, sdp_rpc_cb_t callback);
-void unregister_rpc_service(const SDPInterfaceDescription &request_description, const SDPInterfaceDescription &response_description);
-void _request_response_waiting_callback(const uint8_t *mac_addr, const SDPInterfaceDescription &interface_description, const std::vector<SDPData> &body);
+
+/**
+ * @brief Unregister RPC service from the server.
+ *
+ * @param request_description The description of the request interface
+ */
+void unregister_rpc_service(const SDPInterfaceDescription &request_description);
+
+/**
+ * @brief Get RPC interfaces of other devices. This function will return the list of RPC interfaces of other devices. The scanning process will take a few seconds.
+ *
+ * @param duration The duration of the scanning process (in milliseconds)
+ * @return std::vector<std::tuple<std::string, SDPInterfaceDescription, SDPInterfaceDescription>> The list of RPC interfaces. Each element of the list is a tuple of device name, request interface description, and response interface description.
+ */
+std::vector<std::tuple<std::string, SDPInterfaceDescription, SDPInterfaceDescription>> get_rpc_interfaces(unsigned long duration = 5000);
+
+/**
+ * @brief Call RPC function of other devices. This function will send a request to the device and wait for the response. The response will be stored in the response_body parameter.
+ *
+ * @param mac_addr The MAC address of the device
+ * @param request_interface The description of the request interface
+ * @param request_body The request body
+ * @param response_interface The description of the response interface
+ * @param response_body The response body (output)
+ * @param duration The duration of the waiting process (in milliseconds)
+ * @return true if the request is successful
+ * @return false if the request is failed
+ */
 bool call_sdp_rpc(const uint8_t *mac_addr, const SDPInterfaceDescription &request_interface, const std::vector<SDPData> &request_body, const SDPInterfaceDescription &response_interface, std::vector<SDPData> &response_body, unsigned long duration = 3000);
 
-/*
+//
+void _OnDataRecvRPC(const uint8_t *mac_addr, const uint8_t *data, int data_len);
+bool _broadcast_sdp_rpc_meta_packet(const SDPInterfaceDescription &interface_request, const SDPInterfaceDescription &interface_response);
+void _rpc_meta_frame_broadcast_task(void *parameter);
+
+/**
  * Global variables
- */
+ **/
+std::vector<SDPRPCCallbackEntry> _rpc_server_callbacks;  // RPC server callback list
 
-// RPC Server callbacks
-// This dictionary is used to store the callback for each request and used in _request_response_callback
-// Each element is a tuple of (request interface, response interface, callback)
-std::map<SDPInterfaceDescription, std::tuple<SDPInterfaceDescription, sdp_data_if_recv_cb_t>> _rpc_server_callbacks;
-
-// RPC response callback dictionary
-// This dictionary is used to store the response from RPC response for each request and used in _request_response_callback
-std::map<int32_t, std::tuple<std::string, SDPInterfaceDescription, std::vector<SDPData>>> _rpc_response_callback_dictionary;  // request id -> (server_address, response, response body)
-
-/*
+/**
  * RPC Library
- */
-
+ **/
 bool init_sdp_rpc() {
-  // Register callback for response waiting
-  register_sdp_data_callback(_request_response_waiting_callback);
-  // register_sdp_data_callback(_rcp_server_base_callback);
-  return true;
+  auto result = xTaskCreate(_rpc_meta_frame_broadcast_task, "rpc_meta_frame_broadcast_task", 4096, NULL, 5, NULL);
+  return result == pdPASS;
+  register_sdp_esp_now_recv_callback(_OnDataRecvRPC);
 }
 
-//
-// RPC Server Library
-//
-
-void _server_callback_base(const uint8_t *mac_addr, const std::vector<SDPData> &body, sdp_rpc_cb_t callback, SDPInterfaceDescription request_interface, SDPInterfaceDescription response_interface) {
-  // Get request id
-  int32_t request_id = std::get<int32_t>(body[0]);
-
-  // Get response body
-  std::vector<SDPData> response_body;
-  callback(mac_addr, body, response_body);
-
-  // Send response
-  send_sdp_data_packet(mac_addr, response_interface, response_body);
+bool _broadcast_sdp_rpc_meta_packet(
+    const SDPInterfaceDescription
+        &interface_request,
+    const SDPInterfaceDescription &interface_response) {
+  uint8_t buf[240];
+  std::string request_packet_description = std::get<0>(interface_request);
+  std::string request_serialization_format = std::get<1>(interface_request);
+  std::string response_packet_description = std::get<0>(interface_response);
+  std::string response_serialization_format = std::get<1>(interface_response);
+  generate_rpc_meta_frame(buf, request_packet_description, request_serialization_format, response_packet_description, response_serialization_format);
+  bool success = broadcast_sdp_esp_now_packet(buf, sizeof(buf)) == ESP_OK;
+  return success;
 }
 
-bool register_rpc_service(const SDPInterfaceDescription &request_description, const SDPInterfaceDescription &response_description, sdp_rpc_cb_t callback) {
-  // Check if request and response serialization formats are valid as RPC
-  std::string request_packet_description = std::get<0>(request_description);
-  std::string request_serialization_format = std::get<1>(request_description);
-  std::string response_packet_description = std::get<0>(response_description);
-  std::string response_serialization_format = std::get<1>(response_description);
-  if (request_serialization_format[0] != 'i' or response_serialization_format[0] != 'i') {
-    return false;
+void _rpc_meta_frame_broadcast_task(void *parameter) {
+  while (true) {
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    for (auto &entry : _rpc_server_callbacks) {
+      SDPInterfaceDescription request_interface = std::get<0>(entry);
+      SDPInterfaceDescription response_interface = std::get<1>(entry);
+      _broadcast_sdp_rpc_meta_packet(request_interface, response_interface);
+    }
   }
+}
 
-  // bind callback
-  std::function<void(const uint8_t *, const std::vector<SDPData> &)> callback_bind = std::bind(_server_callback_base, std::placeholders::_1, std::placeholders::_2, callback, request_description, response_description);
-  // Register callback
-  register_sdp_interface_callback(request_description, (void *(const uint8_t *mac_addr,
-                                                               const std::vector<SDPData> &body))callback_bind);
+void _OnDataRecvRPC(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  int packet_type = get_packet_type(data, data_len);
+  if (packet_type == smart_device_protocol::PACKET_TYPE_DATA) {
+    auto packet = parse_packet_as_data_packet(data);
+    SDPInterfaceDescription interface_description = std::get<0>(packet);
+    std::string packet_description = std::get<0>(interface_description);
+    std::string serialization_format = std::get<1>(interface_description);
+    std::vector<SDPData> body = std::get<1>(packet);
+    std::vector<SDPData> response_body;
+    for (auto &entry : _rpc_server_callbacks) {
+      SDPInterfaceDescription request_interface = std::get<0>(entry);
+      SDPInterfaceDescription response_interface = std::get<1>(entry);
+      if (std::get<0>(request_interface) == packet_description and
+          std::get<1>(request_interface) == serialization_format) {
+        std::get<2>(entry)(mac_addr, body, response_body);
+        send_sdp_data_packet(mac_addr, response_interface, response_body);
+      }
+    }
+  }
+}
 
+bool register_rpc_service(const SDPInterfaceDescription &request_description,
+                          const SDPInterfaceDescription &response_description,
+                          sdp_rpc_cb_t callback) {
+  _rpc_server_callbacks.push_back(std::make_tuple(request_description, response_description, callback));
   return true;
 }
 
 void unregister_rpc_service(const SDPInterfaceDescription &request_description) {
+  for (int i = 0; i < _rpc_server_callbacks.size(); i++) {
+    if (std::get<0>(_rpc_server_callbacks[i]) == request_description) {
+      _rpc_server_callbacks.erase(_rpc_server_callbacks.begin() + i);
+      break;
+    }
+  }
 }
-
-//
-// RPC Client
-//
 
 // callback for response waiting
 void _request_response_waiting_callback(const uint8_t *mac_addr,
@@ -135,7 +199,6 @@ void _request_response_waiting_callback(const uint8_t *mac_addr,
   _rpc_response_callback_dictionary[request_id] = std::make_tuple(server_address, response_interface, response_body);
 }
 
-// Send request as SDP Data packet and wait for response from the address
 bool call_sdp_rpc(
     const uint8_t *mac_addr, const SDPInterfaceDescription &request_interface, const std::vector<SDPData> &request_body,
     const SDPInterfaceDescription &response_interface, std::vector<SDPData> &response_body,
