e22cecaad66d6f520587788f3ebcdc4acac43059
Koki Shinjo <sktometometo@gmail.com>
2024-02-07 18:32:44 +0900

    [sdp_rpc_client_example] update

--- sketchbooks/sdp_rpc_client_example/src/main.cpp
+++ sketchbooks/sdp_rpc_client_example/src/main.cpp
@@ -14,30 +14,8 @@ String device_name = "SDP RPC EXAMPLE";
 // ESP-NOW
 uint8_t mac_address[6] = {0};
 
-// SDP RPC Interface
-std::string packet_description_request = "Echo req";
-std::string serialization_format_request = "isifS";
-SDPInterfaceDescription interface_description_request(packet_description_request, serialization_format_request);
-std::string packet_description_response = "Echo res";
-std::string serialization_format_response = "isifS";
-SDPInterfaceDescription interface_description_response(packet_description_response, serialization_format_response);
-
 // Other
 std::vector<SDPData> data;
-
-void rpc_handler(const uint8_t *mac_addr,
-                 const std::vector<SDPData> &request_body,
-                 std::vector<SDPData> &response_body) {
-  std::string mac_str = _convert_mac_address(mac_addr);
-  Serial.printf("RPC Handler called from %s\n", mac_str.c_str());
-  int request_id = std::get<int32_t>(request_body[0]);
-  response_body.clear();
-  response_body.push_back(SDPData(request_id));
-  response_body.push_back(SDPData(std::get<std::string>(request_body[1])));
-  response_body.push_back(SDPData(std::get<int32_t>(request_body[2])));
-  response_body.push_back(SDPData(std::get<float>(request_body[3])));
-  response_body.push_back(SDPData(std::get<std::string>(request_body[4])));
-}
 
 void setup() {
   M5.begin(true, true, true, false);
@@ -66,9 +44,6 @@ void setup() {
   }
   Serial.println("SDP RPC Initialized!");
 
-  // Register RPC handler
-  register_rpc_service(interface_description_request, interface_description_response, rpc_handler);
-
   // Display MAC address
   M5.Lcd.printf("Name: %s\n", device_name.c_str());
   M5.Lcd.printf("ADDR: %2x:%2x:%2x:%2x:%2x:%2x\n", mac_address[0], mac_address[1], mac_address[2], mac_address[3],
@@ -77,8 +52,153 @@ void setup() {
   Serial.printf("ADDR: %2x:%2x:%2x:%2x:%2x:%2x\n", mac_address[0], mac_address[1], mac_address[2], mac_address[3],
                 mac_address[4], mac_address[5]);
   Serial.println("Initialization completed!");
+
+  Serial.print("Input >");
 }
 
+std::vector<std::tuple<std::__cxx11::string, SDPInterfaceDescription, SDPInterfaceDescription>> rpc_interfaces;
+
 void loop() {
-  delay(5000);
+  delay(1000);
+
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.replace("\n", "");
+    command.replace("\r", "");
+    Serial.println(command);
+
+    if (command == "help") {
+      Serial.printf("Commands:\n");
+      Serial.printf("  help: Show this help\n");
+      Serial.printf("  list: List RPC Interfaces\n");
+      Serial.printf("  call: Call RPC Interface\n");
+    } else if (command == "list") {
+      Serial.printf("Getting RPC Interfaces...\n");
+      rpc_interfaces = get_rpc_interfaces();
+      Serial.printf("Done! %d interfaces found\n", rpc_interfaces.size());
+
+      for (const auto& rpc_interface : rpc_interfaces) {
+        const std::string& name = std::get<0>(rpc_interface);
+        const SDPInterfaceDescription& request_description = std::get<1>(rpc_interface);
+        const SDPInterfaceDescription& response_description = std::get<2>(rpc_interface);
+        const std::string& request_packet_description = std::get<0>(request_description);
+        const std::string& request_serialization_format = std::get<1>(request_description);
+        const std::string& response_packet_description = std::get<0>(response_description);
+        const std::string& response_serialization_format = std::get<1>(response_description);
+
+        Serial.printf("=============================\n");
+        Serial.printf("Name: \"%s\"\n", name.c_str());
+        Serial.printf("Request Packet Description: \"%s\"\n", request_packet_description.c_str());
+        Serial.printf("Request Serialization: \"%s\"\n", request_serialization_format.c_str());
+        Serial.printf("Response Packet Description: \"%s\"\n", response_packet_description.c_str());
+        Serial.printf("Response Serialization: \"%s\"\n", response_serialization_format.c_str());
+        Serial.printf("=============================\n");
+      }
+    } else if (command == "call") {
+      Serial.print("Select interface index > ");
+      while (not Serial.available()) {
+        delay(100);
+      }
+      String index_str = Serial.readStringUntil('\n');
+      index_str.replace("\n", "");
+      index_str.replace("\r", "");
+      Serial.println(index_str);
+      int index = index_str.toInt();
+      if (index < 0 or index >= rpc_interfaces.size()) {
+        Serial.printf("Invalid index: %d\n", index);
+        return;
+      }
+      auto rpc_interface = rpc_interfaces[index];
+      const std::string& name = std::get<0>(rpc_interface);
+      const SDPInterfaceDescription& request_description = std::get<1>(rpc_interface);
+      const SDPInterfaceDescription& response_description = std::get<2>(rpc_interface);
+      std::string request_serialization_format = std::get<1>(request_description);
+      std::string response_serialization_format = std::get<1>(response_description);
+      std::vector<SDPData> request_data;
+      request_data.push_back(SDPData(1));
+      std::vector<SDPData> response_data;
+      for (int i = 1; i < request_serialization_format.size(); i++) {
+        char type = request_serialization_format[i];
+        Serial.printf("Input %d/%d (type: %c) > ", i + 1, request_serialization_format.size(), type);
+        while (not Serial.available()) {
+          delay(100);
+        }
+        String input = Serial.readStringUntil('\n');
+        input.replace("\n", "");
+        input.replace("\r", "");
+        Serial.println(input);
+        int32_t val;
+        float fval;
+        bool bval;
+        std::string sval;
+        switch (type) {
+          case 'i':
+            val = input.toInt();
+            Serial.printf("val: %d\n", val);
+            request_data.push_back(SDPData(val));
+            break;
+          case 'f':
+            fval = input.toFloat();
+            Serial.printf("val: %f\n", fval);
+            request_data.push_back(SDPData(fval));
+            break;
+          case '?':
+          case 'b':
+            bval;
+            if (input == "true") {
+              bval = true;
+            } else if (input == "false") {
+              bval = false;
+            } else {
+              Serial.printf("Invalid input: %s\n", input.c_str());
+            }
+            Serial.printf("val: %s\n", bval ? "true" : "false");
+            request_data.push_back(SDPData(bval));
+          case 's':
+          case 'S':
+            sval = input.c_str();
+            Serial.printf("val: %s\n", sval.c_str());
+            request_data.push_back(SDPData(sval));
+            break;
+          default:
+            Serial.printf("Unknown type: %c\n", type);
+            return;
+        }
+      }
+      uint8_t mac_address_list[6] = {0};
+      _convert_mac_address(name, mac_address_list);
+      if (call_sdp_rpc(mac_address_list, request_description, request_data, response_description, response_data)) {
+        Serial.printf("Response: ");
+        for (int i = 0; i < response_serialization_format.size(); i++) {
+          char type = response_serialization_format[i];
+          switch (type) {
+            case 'i':
+              Serial.printf("%d ", std::get<int32_t>(response_data[i]));
+              break;
+            case 'f':
+              Serial.printf("%f ", std::get<float>(response_data[i]));
+              break;
+            case '?':
+            case 'b':
+              Serial.printf("%s ", std::get<bool>(response_data[i]) ? "true" : "false");
+              break;
+            case 's':
+            case 'S':
+              Serial.printf("%s ", std::get<std::string>(response_data[i]).c_str());
+              break;
+            default:
+              Serial.printf("Unknown type: %c\n", type);
+              return;
+          }
+        }
+        Serial.printf("\n");
+      } else {
+        Serial.printf("Failed to call RPC\n");
+      }
+    } else {
+      Serial.printf("Unknown command: \"%s\"\n", command.c_str());
+    }
+
+    Serial.print("Input >");
+  }
 }
